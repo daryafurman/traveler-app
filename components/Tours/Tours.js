15a04941a2630914be06be9a2c3e078db6a971f3
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-02-21 23:12:42 +0100

    slider start

--- components/Tours/Tours.js
+++ components/Tours/Tours.js
@@ -1,26 +1,69 @@
 import styled from "styled-components";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
 
-const ToursContainer = styled.div`
+const SliderContainer = styled.div`
   font-family: "Italiana", sans-serif;
   font-weight: 600;
   font-style: normal;
-  height: 300px;
+  height: 700px;
   background-color: #f4743b;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  flex-direction: column; /* Adjust as needed for your layout */
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transition: transform 1s ease-in-out;
+  transform: translateX(${(props) => (props.active ? "0" : "100%")});
+`;
+
+const TextContainer = styled.div`
+  color: white;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  align-self: center;
+  gap: 10px;
+  width: 100px;
+  padding: 10px;
+  border-radius: 60px;
+  background-color: #70ae6e;
+  color: #beee62;
+  border: none;
+  letter-spacing: 0.4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-family: "Figtree", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
 `;
 
 export default function Tours({ image, country, city, price }) {
   return (
-    <>
-      <ToursContainer>
-        <div>
-          <Image src={image} width={300} height={200} alt={"id"}></Image>
-        </div>
-        <p>
-          {country}, {city}
-        </p>
-        <p>Price: {price}</p>
-      </ToursContainer>
-    </>
+    <SliderContainer>
+      {slidesData.map((slide, index) => (
+        <Slide
+          key={slide.id}
+          active={index === currentSlide}
+          bgColor={slide.bgColor}
+        >
+          <Image src={slide.image} width={300} height={200} alt={slide.title} />
+          <TextContainer>
+            <h2>{slide.title}</h2>
+            <p>{slide.description}</p>
+            <Button>See details</Button>
+          </TextContainer>
+        </Slide>
+      ))}
+    </SliderContainer>
   );
 }
